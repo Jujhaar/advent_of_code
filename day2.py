c626562212d2dda6b33f8f04223562ee44ed2843
jujhaar <singhjujhaar@gmail.com>
2023-12-03 23:33:53 -0800

    cleanup day 2

--- day2.py
+++ day2.py
@@ -1,5 +1,4 @@
-import re
-
+# Part 1
 def sum_of_playable_games(file, max_cubes):
   total = 0
   good_games = set()
@@ -20,7 +19,6 @@ def sum_of_playable_games(file, max_cubes):
       for cubes in turn.split(", "):
         v, k = cubes.split()
         t[k] = int(v)
-      print(t)
 
       skip_game = False
       for color, number in t.items():
@@ -28,7 +26,6 @@ def sum_of_playable_games(file, max_cubes):
           print("color not found in game params", color)
           skip_game = True
         if number > max_cubes[color]:
-          print("too many cubes in this turn", number, max_cubes[color], color)
           skip_game = True
       if skip_game:
         add_to_total = False
@@ -36,8 +33,39 @@ def sum_of_playable_games(file, max_cubes):
     if add_to_total:
       total += game_id
       good_games.add(game_id)
-      print(total, "added to total", game_id)
-      print(good_games)
   return total
 
 print(sum_of_playable_games("day2file.txt", {"red": 12, "green": 13, "blue": 14}))
+
+# Part 2
+def sum_of_all_powers(file):
+  # iterate through file and create Games
+  with open(file, "r") as file:
+    lines = file.readlines()
+  
+  sum_of_powers = 0
+  for line in lines:
+    game_info, turns = line.split(":")
+
+    # get game id
+    game_id = int(game_info.split()[1])
+
+    min_dict = {"red": 0, "green": 0 ,"blue": 0}
+    for turn in turns.split(";"):
+      t = {}
+      for cubes in turn.split(", "):
+        v, k = cubes.split()
+        v = int(v)
+        t[k] = v
+        if k not in min_dict:
+          raise ValueError("unknown color not found in dict:", k)
+        if v > min_dict[k]:
+          min_dict[k] = v
+
+    power = 1
+    for v in min_dict.values():
+      power *= v
+    sum_of_powers += power
+  return sum_of_powers
+
+print(sum_of_all_powers("day2file.txt"))
